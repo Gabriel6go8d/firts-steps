15b69a6a2368ce0156bb0aae086d4548102b5694
Gabriel Gutierrez <35079758+Gabriel6go8d@users.noreply.github.com>
2020-03-29 00:24:17 +0100

    Updates

---
+++ precache-manifest.21e2a20fb77ad922a816251ff60b0a9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372441f6954ef1820be994ef141241d2",
+    "url": "/firts-steps/index.html"
+  },
+  {
+    "revision": "7913133aed098623491d",
+    "url": "/firts-steps/static/css/2.9c2ec2a3.chunk.css"
+  },
+  {
+    "revision": "c67e567580ea0a850e00",
+    "url": "/firts-steps/static/css/main.bad43889.chunk.css"
+  },
+  {
+    "revision": "7913133aed098623491d",
+    "url": "/firts-steps/static/js/2.1252e54b.chunk.js"
+  },
+  {
+    "revision": "26cea779844b95048ab44bfc1acedf77",
+    "url": "/firts-steps/static/js/2.1252e54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67e567580ea0a850e00",
+    "url": "/firts-steps/static/js/main.68967c59.chunk.js"
+  },
+  {
+    "revision": "f11fbc118a8c4bc59370",
+    "url": "/firts-steps/static/js/runtime-main.2546e02e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firts-steps/static/media/logo.5d5d9eef.svg"
+  }
+]);
