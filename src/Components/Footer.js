292b7fa18ab5e99447bdffaf5a0be29824981499
Gabriel Gutierrez <35079758+Gabriel6go8d@users.noreply.github.com>
2020-05-08 13:26:50 +0200

    name

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <div className='container-fluid mt-5 footer'>
             <div className='row justify-content-around text-center text-white'>
                 <div className='col-3 mt-3 mb-3'>
-                    <h3>Firts Steps in React</h3>
+                    <h3>First Steps in React</h3>
                 </div>   
                 <div className='col-3 mt-2 mb-2'>
                 </div> 
--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -7,7 +7,7 @@ function MyNavbar(prop) {
     return (
         <React.Fragment>            
             <Navbar bg="dark" variant='dark' expand="lg" >
-                <Navbar.Brand onClick={() => prop.action('home')} style={{cursor: 'pointer'}}><img src={logo} className="App-logo myLogo" alt="logo"/>Firts Steps</Navbar.Brand>
+                <Navbar.Brand onClick={() => prop.action('home')} style={{cursor: 'pointer'}}><img src={logo} className="App-logo myLogo" alt="logo"/>First Steps</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto" activeKey={prop.active}>
